31b8ec6a987a441ae5b95fa8f6ad922965e3dec4
jenkincstang <66237680+jenkincstang@users.noreply.github.com>
2020-06-17 01:51:21 +0800

    Update main.js

--- 2.function2/main.js
+++ 2.function2/main.js
@@ -1,1 +1,18 @@
-
+function palindrome(message){
+  // wirte your code here
+  var length = message.length;
+  var middle = length+1/2;
+  for(var i=0,j=length-1;i<middle;i++,j--){
+  	if(message.charAt(i)==message.charAt(j)){
+  	}else{
+  		break;
+  	}
+  }
+  if(i>=j){
+  	return true;
+  }else{
+  	return false;
+  }
+}
+console.log(palindrome('hello')); // should return false
+console.log(palindrome('abcba')); // should return true
