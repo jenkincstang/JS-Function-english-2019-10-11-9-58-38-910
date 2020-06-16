d34a994bba39084b75abd701b74efc3ac1b06eb7
jenkincstang <66237680+jenkincstang@users.noreply.github.com>
2020-06-17 01:33:13 +0800

    Update main.js

--- 1.function1/main.js
+++ 1.function1/main.js
@@ -1,1 +1,7 @@
-
+function reverseString(message){
+  // wirte your code here
+  for(var i=message.length-1;i>=0;i--){
+  	process.stdout.write(message.charAt(i));
+  }
+}
+reverseString('hello'); // should return 'olleh'
